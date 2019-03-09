ad2bca5c247e559b8a2031e874de5e592e5b0e53
yu <yuliya.sm7@gmail.com>
2019-03-09 22:37:12 +0300

    fix problem

--- DZ1.c
+++ DZ1.c
@@ -19,86 +19,103 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t check_correct(char *const *const input_lines, size_t n_lines, char **correct_lines);
-size_t input_strings(char ***strings);
-void free_input_strings(char **input_str, size_t n_lines);
 
-int main() {
-    char **input_str;
-    size_t n_lines = input_strings(&input_str);  // ввод массива сток
-    char *correct_str[n_lines];
-    // проверка на соответствие количества открытых и закрытых строк
-    size_t n_out = check_correct(input_str, n_lines, correct_str);
-    for (size_t i = 0; i < n_out; i++) {  // вывод результата
-        printf("%s\n", correct_str[i]);
-    }
-    free_input_strings(input_str, n_lines);  // очистка выделенной памяти
+char **check_correct(char *const *const input_lines, size_t n_lines, size_t *n_out) {
+    if (input_lines == NULL) return NULL;
+    char **correct_lines = calloc(n_lines, sizeof(char *));
+    if (correct_lines == NULL) return NULL;
 
-    return 0;
-}
-
-size_t check_correct(char *const *const input_lines, size_t n_lines, char **correct_lines) {
-    if (input_lines == NULL || correct_lines == NULL)
-        return 0;
     size_t n_correct = 0;
     for (size_t i = 0; i < n_lines; i++) {
-        if (!input_lines[i])  // если указатель ошибочный
-            continue;
-        int bkt_count = 0;  // счетчик скобок
-        for (size_t j = 0; j < strlen(input_lines[i]); j++) {
+        if (!input_lines[i]) continue;  // если указатель ошибочный
+        int bracket_count = 0;
+
+        for (size_t j = 0; j < strlen(input_lines[i]) && bracket_count >= 0; j++) {
             if (input_lines[i][j] == '(') {
-                bkt_count++;
+                bracket_count++;
             } else if (input_lines[i][j] == ')') {
-                bkt_count--;
-            }
-            if (bkt_count < 0) {
-                break; // если закрывающая появилась раньше открывающей - некорректно
+                bracket_count--;
             }
         }
-        if (bkt_count == 0) {
+
+        if (bracket_count == 0) {
             correct_lines[n_correct] = input_lines[i];
             n_correct++;
         }
     }
-    return n_correct;
+    *n_out = n_correct;
+    return correct_lines;
 }
 
-size_t input_strings(char ***strings) {
-    if (!strings)
-        return 0;
-    const size_t n_max = 1024, len_max = 1024;
-    *strings = calloc(n_max, sizeof(char *));
-    if (!(*strings))
-        return 0;
-    size_t n_lines = 0;
 
-    while (1) {  // бесконечный цикл
-        char *str = calloc(len_max, sizeof(char));
-        if (!str)
-            return n_lines;  // оборвать считывание если нет памяти
-
-        fgets(str, len_max, stdin);  // считываю ввод
-
-        if (strlen(str) > 0 && str[strlen(str) - 1] == '\n') {
-            str[strlen(str) - 1] = '\0';  // костыль =)
+char* get_string() {
+    char ch = '\0';
+    size_t n = 0;
+    size_t n_buff = 64;
+    char *str = calloc(n_buff, sizeof(char));
+    if (str == NULL) return NULL;
+    while (scanf("%c", &ch) == 1 && ch != '\n') {
+        str[n] = ch;
+        ++n;
+        if (n >= n_buff) {
+            n_buff *= 2;
+            char *new_ptr = realloc(str, n_buff * sizeof(char));
+            if (new_ptr) str = new_ptr;
+            else break;
         }
-
-        if (strlen(str) == 0) {
-            free(str);
-            break;  // конец считывания
-        }
-        (*strings)[n_lines] = str;
-        n_lines++;
+        str[n] = '\0';
     }
-
-    return n_lines;
+    return str;
 }
+
+char **input_strings(size_t *n_lines) {
+    char **strings = malloc(sizeof(char *));
+    if (strings == NULL || n_lines == NULL) return NULL;
+    size_t n = 0;
+    char *str = get_string();
+
+    while (str != NULL && strlen(str) > 0) {
+        char **new_ptr = realloc(strings, (n + 1) * sizeof(char *));
+        if (new_ptr) strings = new_ptr;
+        else break;
+        strings[n] = str;
+        ++n;
+        str = get_string();  // считываем новую строку
+    }
+    free(str);
+    *n_lines = n;
+    return strings;
+}
+
 
 void free_input_strings(char **input_str, size_t n_lines) {
-    if (!input_str)
-        return;
+    if (!input_str) return;
     for (size_t i = 0; i < n_lines; i++) {
         free(input_str[i]);
     }
     free(input_str);
+}
+
+int main() {
+    size_t n_lines = 0;
+    char **input_str = input_strings(&n_lines);
+    if (input_str == NULL) {
+        printf("[error]");
+        return 0;
+    }
+    // проверка на соответствие количества открытых и закрытых скобок
+    size_t n_out = 0;
+    char **correct_str = check_correct(input_str, n_lines, &n_out);
+    if (correct_str == NULL){
+        printf("[error]");
+        return 0;
+    }
+    // вывод корректных строк
+    for (size_t i = 0; i < n_out; i++) {
+        printf("%s\n", correct_str[i]);
+    }
+
+    free_input_strings(input_str, n_lines);
+    free(correct_str);  // очистка только массива указателей
+    return 0;
 }
